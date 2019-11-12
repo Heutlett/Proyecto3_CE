785ad9abf6f449ca49e8b4125cd09043183dd3bf
kevinlml98 <50761818+kevinlml98@users.noreply.github.com>
2019-11-11 20:12:42 -0600

    Cuarto Commit; Se elimina el paquete maker del api y se coloca en una nueva carpeta ProyectGraphMaker

---
+++ ProjectGraphObjects/graphMaker/src/cr/ac/tec/maker/edge.java
@@ -0,0 +1,38 @@
+package cr.ac.tec.maker;
+
+public class edge {
+
+
+	//_________________ Atributos
+	private String firstPerson;
+	private String lastPerson;
+
+	private edge nextCall;
+
+			private int timeCall;
+
+
+
+	//_________________ Método contructor
+	public edge() {
+
+	}
+
+	public edge(int timeCall, String firstPerson, String lastPerson) {
+
+		this.firstPerson = firstPerson;
+		this.lastPerson = lastPerson;
+		this.timeCall = timeCall;
+
+	}
+
+
+	//_________________ Getters and Setters
+	public int getTimeCall() {
+		return timeCall;
+	}
+
+	public void setNextCall(edge nextCall) {
+		this.nextCall = nextCall;
+	}
+}
